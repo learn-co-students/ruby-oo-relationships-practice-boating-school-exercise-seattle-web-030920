2f92aef3ac8c3831b5a7039372311f079f71514c
sarakhandaker <kttsara@gmail.com>
2020-08-26 12:29:36 -0700

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,16 @@
 class BoatingTest
+    attr_accessor :student, :test_name, :test_status, :instructor
+    @@all=[]
+    def initialize(student, test_name, test_status, instructor)
+        @student=student
+        @instructor=instructor
+        @test_name=test_name
+        @test_status=test_status
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,30 @@
 class Student
+    attr_accessor :first_name
+    @@all=[]
+    def initialize(first_name)
+        @first_name=first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def all_instructors 
+        BoatingTest.all.select{|test| test.student==self}.map(&:instructor).uniq
+    end
+
+    def self.find_student(first_name)
+        @@all.find{|stu| stu.first_name==first_name}
+    end
+
+    def grade_percentage 
+        total=BoatingTest.all.select{|test| test.student==self}
+        total.length/ total.select{|test| test.test_status=="passed"}.to_f.round(2)
+    end
 
 end
